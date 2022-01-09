0adebbf623378cebdb6534b2fd6bf30acf9a7bf5
melanchorilla <dickykamaludin12@gmail.com>
2022-01-09 11:20:56 +0700

    create post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     // protected $fillable = ['title', 'slug', 'excerpt', 'body'];
     protected $guarded = ['id'];
@@ -60,5 +62,14 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
